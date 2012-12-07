ca19abc596aec4ea616fd7fcb3372e3e91178924
Daniel Heath <daniel.r.heath@gmail.com>
2012-12-07 15:20:09 +1100

    Shorten variable name, too annoying to type + broke cane threshold

--- spec/lib/scissors/encryption_spec.rb
+++ spec/lib/scissors/encryption_spec.rb
@@ -9,8 +9,8 @@ describe Scissors::Encryption do
     @subject.extend(Scissors::Encryption)
     @my_data = 'my data'
 
-    @expected_early_signature = "\xCBo8\x80\xE1e\xA0\xF4?\xDC\x1E@\xD50\xA9(2\xD5F\xAD\xC7J\x06\xED\xA09\xD0\x8F\xDD$m\xA2"
-    @expected_later_signature = "4Ai\x89\x95\xEB~\x84\x16\x7F\x9E\x92\x8Bm\x89M8\xBD8\xFD\xF5\xCB\x05Q~;`C\xFF\xD1\x81#"
+    @early_signature = "\xCBo8\x80\xE1e\xA0\xF4?\xDC\x1E@\xD50\xA9(2\xD5F\xAD\xC7J\x06\xED\xA09\xD0\x8F\xDD$m\xA2"
+    @later_signature = "4Ai\x89\x95\xEB~\x84\x16\x7F\x9E\x92\x8Bm\x89M8\xBD8\xFD\xF5\xCB\x05Q~;`C\xFF\xD1\x81#"
   end
 
   describe 'signing arbitrary data' do
@@ -25,15 +25,15 @@ describe Scissors::Encryption do
       end
 
       it 'generates a signature' do
-        @subject.sign(@my_data).should == @expected_early_signature
+        @subject.sign(@my_data).should == @early_signature
       end
 
       it 'verifies the later signature' do
-        @subject.valid?(@my_data, @expected_later_signature).should == true
+        @subject.valid?(@my_data, @later_signature).should == true
       end
 
       it 'verifies the earlier signature' do
-        @subject.valid?(@my_data, @expected_early_signature).should == true
+        @subject.valid?(@my_data, @early_signature).should == true
       end
     end
 
@@ -47,15 +47,15 @@ describe Scissors::Encryption do
       end
 
       it 'generates a different signature' do
-        @subject.sign(@my_data).should == @expected_later_signature
+        @subject.sign(@my_data).should == @later_signature
       end
 
       it 'verifies the later signature' do
-        @subject.valid?(@my_data, @expected_later_signature).should == true
+        @subject.valid?(@my_data, @later_signature).should == true
       end
 
       it 'verifies the earlier signature' do
-        @subject.valid?(@my_data, @expected_early_signature).should == true
+        @subject.valid?(@my_data, @early_signature).should == true
       end
     end
 
